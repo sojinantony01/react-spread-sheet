c4a22dd2d82604b1e1e6083d778a5f9a716d6957
Sojin antony <sojin.antony01@gmail.com>
2024-08-15 17:25:34 +0530

    Revert "Styling (#10)"
    This reverts commit b7839dc22dbd7bc092214e14da60ae64d0d9ce77.

--- src/lib/list/cell.tsx
+++ src/lib/list/cell.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
 import Input from "./input";
-import { useAppSelector } from "../store";
 interface Prop {
   i: number;
   j: number;
@@ -14,12 +13,9 @@ const Cell = (props: Prop) => {
     root: document.getElementsByClassName("sheet-table")[0],
     rootMargin: "100px",
   });
-  const selected = useAppSelector((store) => {
-    return store.list.selected.some(p => p[0] === props.i && p[1] === props.j)
-  });
 
   return (
-    <td ref={ref} className={`${!inView ? "pv-4" : ""} ${selected ? "sheet-selected-td" : ""}`}>
+    <td ref={ref} className={`${!inView ? "pv-4" : ""}`}>
       {inView ? <Input key={`${props.i}-${props.j}`} {...props} /> : " "}
     </td>
   );
