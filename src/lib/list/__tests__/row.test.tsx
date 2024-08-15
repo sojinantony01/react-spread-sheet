eac3ab5977e2c8b87b7c7e9b06b4e999c9f2410c
Sojin antony <sojin.antony01@gmail.com>
2024-08-15 14:42:14 +0530

    Un it test improvements part-2 (#7)

--- src/lib/list/__tests__/row.test.tsx
+++ src/lib/list/__tests__/row.test.tsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../../store";
 import { addData } from "../../reducer";
 import { generateDummyContent } from "../utils";
 import Row from "../row";
 const i = 1;
+
+test("read only row", () => {
+    store.dispatch(addData(generateDummyContent(3, 3)));
+    render(
+      <Provider store={store}>
+        <table>
+          <tbody>
+            <Row key={i} i={i} readonly />
+          </tbody>
+        </table>
+      </Provider>,
+    );
+    expect(screen.getByTestId(`read-only-${i}-0`)).toBeInTheDocument();
+})
 test("row  render", () => {
   store.dispatch(addData(generateDummyContent(3, 3)));
   render(
     <Provider store={store}>
-        <table><tbody><Row key={i} i={i} readonly/></tbody></table>
+        <table><tbody><Row key={i} i={i}/></tbody></table>
     </Provider>
   );
-  expect(screen.getByTestId(`read-only-${i}-0`)).toBeInTheDocument();
+  expect(screen.getByTestId("1-sheet-y-axis")).toBeInTheDocument();
+  fireEvent.click(screen.getByTestId("1-sheet-y-axis"));
+  expect(store.getState().list.selected).toHaveLength(3);
 });
