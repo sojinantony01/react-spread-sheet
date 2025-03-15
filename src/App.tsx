ed59a6b518569333e8fcdf367f0e7baa7813b44a
Sojin Antony <sojin.antony01@gmail.com>
2025-03-15 14:25:19 +0530

    Solutions updated

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const createData = () => {
   const val: any[][] = [];
   for (let i = 0; i < 1000 ; i++) {
     val.push(
-      Array.from({ length: 100 }, () => ({
+      Array.from({ length: 40 }, () => ({
         value: Math.floor(Math.random() * 10),
       }))
     );
